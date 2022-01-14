b969e3ca94d74fbd0c127d47d49e558cb1966417
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2022-01-14 11:06:51 -0600

    Uppdated interval selector and removed obsolete methods

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,13 +11,12 @@ export class SidebarComponent implements OnInit {
   @Output() historicalSearch = new EventEmitter();
   @Output() darkModeToggle = new EventEmitter();
   symbols: string[] = ['F', 'T'];
-  interval: number = 0;
+  interval: string = '';
   start: string = '';
   end: string = '';
   dark: boolean = false;
   expand: boolean = false;
   activeSymbols: boolean[] = [true,true,false,false,false,false,false,false,false,false];
-  activeIntervals: boolean[] = [false, false, false, false];
   
   constructor() { }
 
@@ -57,40 +56,6 @@ export class SidebarComponent implements OnInit {
   }
 
   /**
-   * On click the id of the clicked event is checked against the value of the interval 
-   * property
-   * If they are the same, interval is set to zero.  If not, interval is set to the id
-   * Active intervals is updated depending on the value of interval to allow for CSS to 
-   * show the active interval button
-   * @param event - Event
-   * @returns - void 
-   */
-  handleIntervalClick(event: Event): void {
-    this.interval = Number((event.target as HTMLElement).id) === this.interval 
-      ? 0 : Number((event.target as HTMLElement).id);
-    console.log(this.interval);
-    console.log(`Start: ${this.start}`);
-    console.log(`End: ${this.end}`);
-    switch(this.interval) {
-      case 5:
-        this.activeIntervals = [true, false, false, false];
-        break;
-      case 15:
-        this.activeIntervals = [false, true, false, false];
-        break;
-      case 60:
-        this.activeIntervals = [false, false, true, false];
-        break;
-      case 1440:
-        this.activeIntervals = [false, false, false, true];
-        break;
-      default:
-        this.activeIntervals = [false, false, false, false];
-        break;
-    }
-  }
-
-  /**
    * On submit the symbols, start, end, and interval are all checked to see if they are
    * valid
    * If so, the start and end property are converted into timestamps  
@@ -108,16 +73,16 @@ export class SidebarComponent implements OnInit {
       && this.interval) {
         const startTimestamp = Date.parse(this.start);
         const endTimestamp = Date.parse(this.end);
+        const intervalInt = Number(this.interval);
         this.historicalSearch.emit({
           symbols: this.symbols,
           start: startTimestamp,
           end: endTimestamp,
-          interval: this.interval
+          interval: intervalInt
         });
         this.start = '';
         this.end = '';
-        this.interval = 0;
-        this.activeIntervals = [false, false, false, false];    
+        this.interval = ''; 
       } else {
       console.log('something is wrong')
     }
