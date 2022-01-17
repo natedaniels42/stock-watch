4b71ceba7f2352c6693ba4eeac8c6e7488f5e126
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2022-01-17 15:57:40 -0600

    Fixed PORT in server for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/StockWatch'));
 
@@ -10,4 +11,4 @@ app.get('/*', (req, res) => {
 });
 
 
-app.listen(8080, console.log(`Server is running on port: 8080`));
+app.listen(PORT, console.log(`Server is running on port: ${PORT}`));
